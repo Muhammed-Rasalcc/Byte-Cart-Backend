7bc396bb788c72559241fbe13eda66fa00b914af
Muhammed-Rasalcc <rasalcc9@gmail.com>
2025-04-23 19:16:19 +0530

    Something new

--- index.js
+++ index.js
@@ -5,7 +5,8 @@ const app = express()
 require('dotenv').config()
 const cookieParser = require('cookie-parser')
 const bodyParser = require('body-parser');
-const port = process.env.port || 5000
+const port = process.env.PORT || 5000
+
 
 //middlewire setting
 app.use(express.json({limit: "25mb"}));
