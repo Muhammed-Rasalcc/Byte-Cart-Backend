7851a467033f5347eff01e3fdbf917cec9af7d59
Muhammed-Rasalcc <rasalcc9@gmail.com>
2025-04-23 21:42:21 +0530

    url update

--- index.js
+++ index.js
@@ -14,7 +14,7 @@ app.use(cookieParser());
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: true }));
 app.use(cors({
-    origin: 'http://localhost:5173',
+    origin: 'https://byte-cart-final.vercel.app/',
     credentials: true
 }))
 
